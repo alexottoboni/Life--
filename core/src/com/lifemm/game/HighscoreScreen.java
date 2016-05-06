2410c5f5245cd0e4501fd43b0c06535099dcff1b
wyattjsmith1 <taysoftware@gmail.com>
2016-05-05 17:38:47 -0700

    551034 - Fixed some SonarQube issues

--- core/src/com/lifemm/game/HighscoreScreen.java
+++ core/src/com/lifemm/game/HighscoreScreen.java
@@ -22,12 +22,6 @@ public class HighscoreScreen implements Screen {
    BitmapFont font;
   
    public HighscoreScreen (final LifeMM game) {
-      this.game = game;
-      try {
-         highscores = ScoreAPI.getInstance().getScores();
-      } catch (IOException e) {
-
-      }
       // Generate the font we use
       FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("GROBOLD.ttf"));
       FreeTypeFontParameter parameter = new FreeTypeFontParameter();
@@ -35,23 +29,33 @@ public class HighscoreScreen implements Screen {
       parameter.borderColor = Color.BLACK;
       parameter.borderWidth = 2;
       font = generator.generateFont(parameter);
+      this.game = game;
+      try {
+        highscores = ScoreAPI.getInstance().getScores();
+      } catch (IOException e) {
+        highscores = null;
+      }
    }
 
    @Override
    public void render (float delta) {
       if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
-         game.setScreen(new MainMenu(game));
-         dispose();
+        game.setScreen(new MainMenu(game));
+        dispose();
       }
       Gdx.gl.glClearColor(1, 1, 1, 1);
       Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
       game.batch.begin();
-      font.draw(game.batch, highscores.get(0).toString(), 1440/2 - 100, 700);
-      font.draw(game.batch, highscores.get(1).toString(), 1440/2 - 100, 600);
-      font.draw(game.batch, highscores.get(2).toString(), 1440/2 - 100, 500);
-      font.draw(game.batch, highscores.get(3).toString(), 1440/2 - 100, 400);
-      font.draw(game.batch, highscores.get(4).toString(), 1440/2 - 100, 300);
-      game.batch.end();
+      if (highscores == null) {
+        font.draw(game.batch, "Failed to get High Scores.", 1440/2 - 100, 700);
+      } else {
+        font.draw(game.batch, highscores.get(0).toString(), 1440/2 - 100, 700);
+        font.draw(game.batch, highscores.get(1).toString(), 1440/2 - 100, 600);
+        font.draw(game.batch, highscores.get(2).toString(), 1440/2 - 100, 500);
+        font.draw(game.batch, highscores.get(3).toString(), 1440/2 - 100, 400);
+        font.draw(game.batch, highscores.get(4).toString(), 1440/2 - 100, 300);
+        game.batch.end();
+      }
    }
 
 
--- desktop/src/com/lifemm/game/desktop/DesktopLauncher.java
+++ desktop/src/com/lifemm/game/desktop/DesktopLauncher.java
@@ -5,12 +5,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.lifemm.game.LifeMM;
 
 public class DesktopLauncher {
+
+    private DesktopLauncher() {
+
+    }
+    
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
       config.title = "Life--";
       config.width = 1440;
       config.height = 810;
 		LwjglApplication game = new LwjglApplication(new LifeMM(), config);
-      System.out.println(game.toString());
 	}
 }
