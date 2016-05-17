03a5c15b1e928c67d0ac584efbe7e5c481dc24d8
Alex Ottoboni <alexottoboni@gmail.com>
2016-05-16 23:32:06 -0700

    Added more things to cleanup script Issues Affected: 553543

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-rm -rf .gradle/2.10/taskArtifacts/*
 rm -rf core/.settings
 rm -rf desktop/.settings
 rm -rf .settings
+rm -rf core/bin
+rm -rf desktop/bin
 gradle cleanEclipse
+rm -rf .gradle
