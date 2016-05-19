7219f56a7ea572bceed36403326f3c4caf7f2380
wyattjsmith1 <taysoftware@gmail.com>
2016-05-19 16:19:34 -0700

    556257 Added JUnit tests for Score object. Removed temporary WorkingTest.java

---
+++ core/test/com/lifemm/game/TestScore.java
@@ -0,0 +1,44 @@
+package com.lifemm.game;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import com.lifemm.game.Score;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests the Score object.
+ * @author Wyatt Smith
+ */
+
+public class TestScore {
+    @Test
+    public void testGetScoreGeneral() {
+        Score score = new Score("Wyatt Smith", 10000);
+        assertEquals(10000, score.getScore());
+    }
+
+    @Test
+    public void testGetNameGeneral() {
+        Score score = new Score("Wyatt Smith", 10000);
+        assertEquals("Wyatt Smith", score.getName());
+    }
+
+    @Test
+    public void testGetNameNull() {
+        Score score = new Score(null, 10000);
+        assertEquals(null, score.getName());
+    }
+
+    @Test
+    public void testToStringGeneral() {
+        Score score = new Score("Wyatt Smith", 10000);
+        assertEquals("Wyatt Smith: 10000", score.toString());
+    }
+
+    @Test
+    public void testToStringNull() {
+        Score score = new Score(null, 10000);
+        assertEquals("null: 10000", score.toString());
+    }
+}
