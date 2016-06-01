cb4f06e1d4f7e26af0d5dd453085ab21f993f907
Alex Ottoboni <alexottoboni@gmail.com>
2016-05-31 17:21:24 -0700

    Added loop test for Waldo's state changing
    Issues Affected: 556257

--- core/test/com/lifemm/game/TestWaldo.java
+++ core/test/com/lifemm/game/TestWaldo.java
@@ -43,4 +43,40 @@ public class TestWaldo extends TestCase {
       waldo.decrementLives();
       assertEquals(2, waldo.getLives());
    }
+
+   // Loop is never entered
+   public void TestUpdateStateLoopInitial() {
+      Waldo waldo = new Waldo();
+      assertEquals(2, waldo.getState()); 
+   }
+
+   // Loop is entered once
+   public void TestUpdateStateLoopOnce() {
+      Waldo waldo = new Waldo();
+      waldo.setState(4);
+      for (int i = 0; i < 1; i++) {
+         waldo.updateState();
+      }
+      assertEquals(2, waldo.getState());
+   }
+
+   // Test loop at the boundary of state change
+   public void TestUpdateStateLoopBoundary() {
+      Waldo waldo = new Waldo();
+      waldo.setState(4);
+      for (int i = 0; i < 101; i++) {
+         waldo.updateState();
+      }
+      assertEquals(2, waldo.getState());
+   }
+
+   // Test loop past the boundary of state change
+   public void TestUpdateStateLoopPastBoundary() {
+      Waldo waldo = new Waldo();
+      waldo.setState(4);
+      for (int i = 0; i < 150; i++) {
+         waldo.updateState();
+      }
+      assertEquals(2, waldo.getState());
+   }
 }
